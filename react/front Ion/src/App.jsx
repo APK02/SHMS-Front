492da9ae46c168455ad9049880144204227a1f64
Cazacu Ion <cazacioc51@gmail.com>
2024-05-30 02:51:40 +0300

    Same doar ca de la Ion

---
+++ react/front Ion/src/App.jsx
@@ -0,0 +1,29 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { SignIn } from './Pages/SignIn';
+import { SignUp } from './Pages/SignUp';
+import { Account } from './Pages/Account';
+import { AddHousehold } from './Pages/AddHousehold';
+import { HouseholdProvider } from './HouseholdContext';
+import { CodeEmailPage } from './Pages/CodeEmail';
+import { PreferencesAndAllergens } from './Pages/PreferencesAndAllergens';
+import { Household } from './Pages/Household';
+
+export function App() {
+  return (
+    <HouseholdProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/addhousehold" element={<AddHousehold />} />
+          <Route path="/codeemail" element={<CodeEmailPage />} />
+          <Route path="/preferences" element={<PreferencesAndAllergens />} />
+          <Route path="/household" element={<Household />} />
+        </Routes>  
+      </Router>    
+    </HouseholdProvider>
+  );
+}
